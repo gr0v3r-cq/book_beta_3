5d85e2007329d161adfbc87663af97669b3325e1
gr0v3r <g.cq143@gmail.com>
2020-01-13 00:46:28 -0400

    first commi book_beta_3

---
+++ onboarding/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Applications/flutter/flutter_1.3.8"
+export "FLUTTER_APPLICATION_PATH=/Users/gr0v3r/Downloads/flutter-ui-concepts/onboarding"
+export "FLUTTER_TARGET=/Users/gr0v3r/Downloads/flutter-ui-concepts/onboarding/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Applications/flutter/flutter_1.3.8/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
